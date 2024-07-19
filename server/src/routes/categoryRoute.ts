623132d0756f173da27673a5576a338a39d34238
Lê Thành An <137029600+lethanhan2001@users.noreply.github.com>
2024-07-19 08:42:47 +0700

    fix: role

--- server/src/routes/categoryRoute.ts
+++ server/src/routes/categoryRoute.ts
@@ -1,5 +1,8 @@
 import express from "express";
-import { verifyTokenAndAdmin } from "../middleware/auth";
+import {
+  verifyTokenAndAdmin,
+  verifyTokenAndAuthorization,
+} from "../middleware/auth";
 import {
   createCategoryProduct,
   deleteCategoryProduct,
@@ -11,12 +14,12 @@ const router = express.Router();
 
 router.post(`/`, verifyTokenAndAdmin, createCategoryProduct);
 
-router.get(`/`, verifyTokenAndAdmin, getCategoryProduct);
+router.get(`/`, verifyTokenAndAuthorization, getCategoryProduct);
 
 router.delete(`/:id`, verifyTokenAndAdmin, deleteCategoryProduct);
 
 router.patch(`/:id`, verifyTokenAndAdmin, updateCategoryProduct);
 
-router.get(`/:id`, verifyTokenAndAdmin, getDetailCategoryProduct);
+router.get(`/:id`, verifyTokenAndAuthorization, getDetailCategoryProduct);
 
 export default router;
