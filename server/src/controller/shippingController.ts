b3056f408d328c931794e8d3a4ccb5d171939610
Lê Thành An <137029600+lethanhan2001@users.noreply.github.com>
2024-07-31 20:18:47 +0700

    fix: improve order

--- server/src/controller/shippingController.ts
+++ server/src/controller/shippingController.ts
@@ -4,148 +4,148 @@ import GeneralDepotModel from "../model/GeneralDepotModel";
 import ProductModel from "../model/ProductModel";
 import TransactionModel from "../model/TransactionModel";
 
-// async function createShippets(req: Request, res: Response) {
-//   try {
-//     const {
-//       fromGeneralId,
-//       toGeneralId,
-//       products,
-//       transferDate,
-//       deliveryDate,
-//       code,
-//     } = req.body;
-
-//     if (!fromGeneralId || !toGeneralId || !products || products.length === 0) {
-//       res.status(403).json("Invalid transfer data provided");
-//     }
-
-//     const mainWarehouse = await GeneralDepotModel.findOne({
-//       type: "main",
-//       _id: fromGeneralId,
-//     });
-//     const subWarehouse = await GeneralDepotModel.findOne({
-//       type: "sub",
-//       _id: toGeneralId,
-//     });
-
-//     if (!mainWarehouse || !subWarehouse) {
-//       throw new Error("Invalid warehouse selection for transfer");
-//     }
-
-//     const updatePromises = products.map(async (product: any) => {
-//       const { productId, inventory_number } = product;
-
-//       if (!productId || !inventory_number) {
-//         return res.status(400).json({ message: "Missing product details" });
-//       }
-
-//       const existingProduct = await ProductModel.findById(productId);
-//       const existGeneral = await GeneralDepotModel.findOne({
-//         type: "sub",
-//         _id: req.body.toGeneralId,
-//       });
-
-//       if (!existingProduct) {
-//         return res.status(400).json({ message: "Invalid product ID" });
-//       }
-
-//       const mainProduct = await ProductModel.findOne({
-//         _id: productId,
-//         generalId: mainWarehouse._id,
-//       });
-//       const subProduct = await ProductModel.findOne({
-//         generalId: subWarehouse._id,
-//         _id: productId,
-//       });
-
-//       if (mainProduct) {
-//         mainProduct.inventory_number -= inventory_number;
-//         await mainProduct.save();
-//       }
-
-//       if (subProduct) {
-//         subProduct.inventory_number += +inventory_number;
-//         await subProduct.save();
-//       } else {
-//         const transferredProduct = {
-//           productId,
-//           inventory_number,
-//         };
-
-//         const newSubProduct = new ShippingWarehouseModel({
-//           code,
-//           products: transferredProduct,
-//           toGeneralId: subWarehouse._id,
-//           fromGeneralId: mainWarehouse._id,
-//           deliveryDate,
-//           transferDate,
-//           status: "pending",
-//         });
-//         await newSubProduct.save();
-//       }
-//       if (existGeneral?.type === "sub") {
-//         const newProduct = new ProductModel({
-//           name_product: existingProduct.name_product,
-//           code: existingProduct.code,
-//           generalId: existGeneral?._id,
-//           manager: req.body.manager,
-//           type: existingProduct.type,
-//           unit: existingProduct.unit,
-//           import_price: existingProduct.import_price,
-//           export_price: existingProduct.export_price,
-//           inventory_number: inventory_number,
-//           status: "stocking",
-//           img: existingProduct.img,
-//           desc: existingProduct.desc,
-//         });
-
-//         await newProduct.save();
-//       } else {
-//         await ProductModel.findOneAndUpdate(
-//           { _id: productId, generalId: existingProduct?.generalId },
-//           { $inc: { inventory_number } },
-//           { upsert: true, new: true }
-//         );
-//       }
-//     });
-
-//     await Promise.all(updatePromises);
-
-//     const totalQuantity = products.reduce(
-//       (acc: number, product: any) => acc + Number(product.inventory_number),
-//       0
-//     );
-
-//     const newTransferOrder = new ShippingWarehouseModel({
-//       code,
-//       fromGeneralId: mainWarehouse._id,
-//       toGeneralId: subWarehouse._id,
-//       products,
-//       transferDate,
-//       totalQuantity,
-//       deliveryDate: new Date().toISOString(),
-//       status: "pending",
-//     });
-
-//     res.status(200).json(newTransferOrder);
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json(error);
-//   }
-// }
-
-const createShippets = async (req: Request, res: Response) => {
-  const shippings = new ShippingWarehouseModel({
-    ...req.body,
-  });
-
+async function createShippets(req: Request, res: Response) {
   try {
-    await shippings.save();
-    res.status(200).json(shippings);
+    const {
+      fromGeneralId,
+      toGeneralId,
+      products,
+      transferDate,
+      deliveryDate,
+      code,
+    } = req.body;
+
+    if (!fromGeneralId || !toGeneralId || !products || products.length === 0) {
+      res.status(403).json("Invalid transfer data provided");
+    }
+
+    const mainWarehouse = await GeneralDepotModel.findOne({
+      type: "main",
+      _id: fromGeneralId,
+    });
+    const subWarehouse = await GeneralDepotModel.findOne({
+      type: "sub",
+      _id: toGeneralId,
+    });
+
+    if (!mainWarehouse || !subWarehouse) {
+      throw new Error("Invalid warehouse selection for transfer");
+    }
+
+    const updatePromises = products.map(async (product: any) => {
+      const { productId, inventory_number } = product;
+
+      if (!productId || !inventory_number) {
+        return res.status(400).json({ message: "Missing product details" });
+      }
+
+      const existingProduct = await ProductModel.findById(productId);
+      const existGeneral = await GeneralDepotModel.findOne({
+        type: "sub",
+        _id: req.body.toGeneralId,
+      });
+
+      if (!existingProduct) {
+        return res.status(400).json({ message: "Invalid product ID" });
+      }
+
+      const mainProduct = await ProductModel.findOne({
+        _id: productId,
+        generalId: mainWarehouse._id,
+      });
+      const subProduct = await ProductModel.findOne({
+        generalId: subWarehouse._id,
+        _id: productId,
+      });
+
+      if (mainProduct) {
+        mainProduct.inventory_number -= inventory_number;
+        await mainProduct.save();
+      }
+
+      if (subProduct) {
+        subProduct.inventory_number += +inventory_number;
+        await subProduct.save();
+      } else {
+        const transferredProduct = {
+          productId,
+          inventory_number,
+        };
+
+        const newSubProduct = new ShippingWarehouseModel({
+          code,
+          products: transferredProduct,
+          toGeneralId: subWarehouse._id,
+          fromGeneralId: mainWarehouse._id,
+          deliveryDate,
+          transferDate,
+          status: "pending",
+        });
+        await newSubProduct.save();
+      }
+      if (existGeneral?.type === "sub") {
+        const newProduct = new ProductModel({
+          name_product: existingProduct.name_product,
+          code: existingProduct.code,
+          generalId: existGeneral?._id,
+          manager: req.body.manager,
+          type: existingProduct.type,
+          unit: existingProduct.unit,
+          import_price: existingProduct.import_price,
+          export_price: existingProduct.export_price,
+          inventory_number: inventory_number,
+          status: "stocking",
+          img: existingProduct.img,
+          desc: existingProduct.desc,
+        });
+
+        await newProduct.save();
+      } else {
+        await ProductModel.findOneAndUpdate(
+          { _id: productId, generalId: existingProduct?.generalId },
+          { $inc: { inventory_number } },
+          { upsert: true, new: true }
+        );
+      }
+    });
+
+    await Promise.all(updatePromises);
+
+    const totalQuantity = products.reduce(
+      (acc: number, product: any) => acc + Number(product.inventory_number),
+      0
+    );
+
+    const newTransferOrder = new ShippingWarehouseModel({
+      code,
+      fromGeneralId: mainWarehouse._id,
+      toGeneralId: subWarehouse._id,
+      products,
+      transferDate,
+      totalQuantity,
+      deliveryDate: new Date().toISOString(),
+      status: "pending",
+    });
+
+    res.status(200).json(newTransferOrder);
   } catch (error) {
-    res.status(400).json(error);
+    console.log(error);
+    res.status(500).json(error);
   }
-};
+}
+
+// const createShippets = async (req: Request, res: Response) => {
+//   const shippings = new ShippingWarehouseModel({
+//     ...req.body,
+//   });
+
+//   try {
+//     await shippings.save();
+//     res.status(200).json(shippings);
+//   } catch (error) {
+//     res.status(400).json(error);
+//   }
+// };
 
 const getShippets = async (req: Request, res: Response) => {
   try {
